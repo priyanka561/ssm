42b33c9795142d96431bf9bbe85f3dd160d6faeb
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-09-15 18:20:58 +0530

    Update ubuntu_ssm.rb

--- recipes/ubuntu_ssm.rb
+++ recipes/ubuntu_ssm.rb
@@ -5,6 +5,6 @@ case node['platform']
 when 'ubuntu'
   execute 'apt-get update' do
     command 'apt-get update -y'
-    command 'wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb .'
+    command 'wget --no-check-certificate https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb .'
   end
 end
